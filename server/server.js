74fba8ce2b755103d7fd77bc6a118b8195f5ad31
Ryan Maas <rdmjobs@live.com>
2021-05-28 09:11:48 -0500

    Cleaned up errors.  Working server.

--- server/server.js
+++ server/server.js
@@ -1,17 +1,18 @@
 const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000;
 const booksRouter = require('./routes/book.router.js');
 
-const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('server/public'))
 
+// ROUTES
 app.use('/books', booksRouter);
 
 // Serve back static files by default
-app.use(express.static('server/public'))
 
 // Start listening for requests on a specific port
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log('listening on port', PORT);
 });
